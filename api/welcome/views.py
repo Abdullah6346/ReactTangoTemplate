4389cab24dad71b5cc08b52b598f19729b4cbffb
Muhammad Abdullah <muhmmadabdullah6346@gmail.com>
2025-05-23 10:51:21 +0500

    feat: remove personalized welcome endpoint for simplification

--- api/welcome/views.py
+++ api/welcome/views.py
@@ -10,16 +10,7 @@ def general_welcome(request):
     """
     return Response({"message": "Welcome to our Awesome API!"})
 
-@api_view(['GET'])
-def personalized_welcome(request, name):
-    """
-    A personalized welcome message.
-    """
-    if not name: # Should not happen with URL config, but good for robustness
-        return Response({"error": "Name cannot be empty"}, status=status.HTTP_400_BAD_REQUEST)
-    
-    # You could add more logic here, e.g., check for known names, etc.
-    return Response({"message": f"Hello, {name}! Welcome aboard."})
+
 
 # Example of a more complex welcome that could show API info
 @api_view(['GET'])
@@ -33,7 +24,6 @@ def api_info(request):
         "description": "An API to provide warm welcomes.",
         "endpoints": {
             "/welcome/": "General welcome message.",
-            "/welcome/{name}/": "Personalized welcome for a given name.",
             "/welcome/info/": "This information."
         }
     }
